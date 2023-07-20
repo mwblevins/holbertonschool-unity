1d6de8f1ccc7b73344b97f26f2a19fdbedc92036
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-07-20 13:51:10 -0500

    Final Time fix

--- unity-assets_ui/Assets/Scripts/Timer.cs
+++ unity-assets_ui/Assets/Scripts/Timer.cs
@@ -14,7 +14,7 @@ public class Timer : MonoBehaviour
         elapsedTime = 0f;
     }
 
-    void OnDisable()
+    public void OnDisable()
     {
         TimerText.color = Color.green;
         TimerText.fontSize = 80;
@@ -24,7 +24,6 @@ public class Timer : MonoBehaviour
     {
         elapsedTime += Time.deltaTime;
 
-        // Calculate minutes, seconds, and milliseconds
         int minutes = (int)(elapsedTime / 60f);
         int seconds = (int)(elapsedTime % 60f);
         int milliseconds = (int)((elapsedTime * 100f) % 100f);
@@ -37,6 +36,7 @@ public class Timer : MonoBehaviour
         if (winCanvas != null && finalTime != null)
         {
             finalTime.text = "Finish Time: " + elapsedTime.ToString("F2");
+            TimerText.gameObject.SetActive(false);
         }
     }
 }
