34f1e884899e0928d2f40a4371eaef12d6510073
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-07-31 13:27:31 -0500

    Jumpin

--- unity-animation/Assets/Scripts/PlayerController.cs
+++ unity-animation/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rotationSpeed = 100f;
-    public float jumpForce = 5f;
+    public float jumpForce = 7f;
     private Rigidbody rb;
     public Transform startPosition;
     public float respawnHeight = -10f;
     public float respawnOffset = 2f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
 
     private Animator animator;
 
@@ -24,30 +27,32 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalRotation = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
-
         // Apply rotation based on 'A' and 'D' input
         transform.Rotate(0f, horizontalRotation * rotationSpeed * Time.deltaTime, 0f);
-
         // Calculate the movement direction based on the current rotation
         Vector3 movementDirection = transform.forward * verticalMovement;
         Vector3 movement = movementDirection * moveSpeed;
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
-        // Set up Bool for when character is moving
-        if (movementDirection != Vector3.zero)
+        // Set up Bool for when character is moving. Idle to Running
+        if (movementDirection != Vector3.zero && IsGrounded())
         {
             animator.SetBool("IsMoving", true);
         }
-
         else
         {
             animator.SetBool("IsMoving", false);
         }
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetBool("IsJumping", true);
         }
-
+        else
+        {
+            animator.SetBool("IsJumping", false);
+        }
+        //Respawn when falling off edge
         if (transform.position.y < respawnHeight)
         {
             rb.velocity = Vector3.zero;
@@ -55,5 +60,9 @@ public class PlayerController : MonoBehaviour
             transform.position = startPosition.position + Vector3.up * respawnOffset;
             rb.useGravity = true;
         }
+    }
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .2f, ground);
     }
 }
