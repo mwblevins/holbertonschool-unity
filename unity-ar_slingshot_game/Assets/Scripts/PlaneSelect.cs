c8cd4ee896be05a113eefe620455b0cf48ae7bbd
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-11-16 13:28:35 -0600

    Update to splitting scripts for better functionality

--- unity-ar_slingshot_game/Assets/Scripts/PlaneSelect.cs
+++ unity-ar_slingshot_game/Assets/Scripts/PlaneSelect.cs
@@ -5,17 +5,15 @@ public class PlaneSelect : MonoBehaviour
 {
     public ARRaycastManager raycastManager;
     public ARPlaneManager planeManager;
-    public GameObject targetPrefab;
-    public int numberOfTargets = 5;
     public Canvas startCanvas;
-
     private bool planeSelectionDone = false;
     public StartButtonController startButtonController;
     public AmmoController ammoController;
+    private bool planeSelectEnabled = true;
 
     void Update()
     {
-        if (!planeSelectionDone && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (planeSelectEnabled && !planeSelectionDone && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
@@ -31,35 +29,16 @@ public class PlaneSelect : MonoBehaviour
                     {
                         p.gameObject.SetActive(false);
                     }
-
                     plane.gameObject.SetActive(true);
                     planeSelectionDone = true;
-
                     planeManager.enabled = false;
                     raycastManager.enabled = false;
-
-                    startButtonController.ShowStartButton();
-
-                    for (int i = 0; i < numberOfTargets; i++)
-                    {
-                        Vector3 randomPosition = GetRandomPositionOnPlane(plane);
-                        Instantiate(targetPrefab, randomPosition, Quaternion.identity);
-                    }
-                    ammoController.ShowAmmo();
+                    StartButtonController.ShowStartButton();
+                    TargetBehavior.SpawnTargets();
+                    AmmoController.ShowAmmo();
+                    planeSelectEnabled = false;
                 }
             }
         }
-    }
-
-    private Vector3 GetRandomPositionOnPlane(ARPlane plane)
-    {
-        Vector3 planeCenter = plane.transform.position;
-        Vector3 planeExtents = new Vector3(plane.size.x / 2, 0, plane.size.y / 2);
-
-        float randomX = Random.Range(-planeExtents.x, planeExtents.x);
-        float randomZ = Random.Range(-planeExtents.z, planeExtents.z);
-        float yPosition = planeCenter.y;
-
-        return new Vector3(randomX + planeCenter.x, yPosition, randomZ + planeCenter.z);
     }
 }
--- unity-ar_slingshot_game/Assets/Scripts/TargetBehavior.cs
+++ unity-ar_slingshot_game/Assets/Scripts/TargetBehavior.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class TargetBehavior : MonoBehaviour
 {
+    public GameObject targetPrefab;
+    public int numberOfTargets = 5;
     public float minScale = 0.2f;
-    public float maxScale = .5f;
+    public float maxScale = 0.5f;
     public float movementSpeed = 1.0f;
     public bool moveInXDirection = true;
     public bool moveInZDirection = true;
@@ -21,5 +23,29 @@ public class TargetBehavior : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, 0, moveZ) * movementSpeed * Time.deltaTime;
         transform.Translate(movement, Space.World);
+    }
+
+    public static void SpawnTargets(int numberOfTargets, ARPlane plane)
+    {
+        for (int i = 0; i < numberOfTargets; i++)
+        {
+            Vector3 randomPosition = GetRandomPositionOnPlane(plane);
+            Instantiate(targetPrefab, randomPosition, Quaternion.identity);
+        }
+    }
+    csharp
+    private Vector3 GetRandomPositionOnPlane(ARPlane plane, float margin = 0f, float padding = 0f)
+    {
+        Vector3 planeCenter = plane.transform.position;
+        Vector3 planeExtents = new Vector3((plane.size.x / 2) - margin, 0, (plane.size.y / 2) - margin);
+
+        // Ensure that padding does not exceed the adjusted extents
+        padding = Mathf.Min(padding, planeExtents.x, planeExtents.z);
+
+        float randomX = Random.Range(-planeExtents.x + padding, planeExtents.x - padding);
+        float randomZ = Random.Range(-planeExtents.z + padding, planeExtents.z - padding);
+        float yPosition = planeCenter.y;
+
+        return new Vector3(randomX + planeCenter.x, yPosition, randomZ + planeCenter.z);
     }
 }
