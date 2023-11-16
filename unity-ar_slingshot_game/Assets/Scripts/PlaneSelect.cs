68d5496f551dd567d762a45f0af3bedef917b0c1
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-11-16 14:46:09 -0600

    Fixed compile error and moved around the functions so that they work

--- unity-ar_slingshot_game/Assets/Scripts/PlaneSelect.cs
+++ unity-ar_slingshot_game/Assets/Scripts/PlaneSelect.cs
@@ -3,17 +3,18 @@ using UnityEngine.XR.ARFoundation;
 
 public class PlaneSelect : MonoBehaviour
 {
+    public GameObject targetPrefab;
     public ARRaycastManager raycastManager;
     public ARPlaneManager planeManager;
     public Canvas startCanvas;
     private bool planeSelectionDone = false;
     public StartButtonController startButtonController;
     public AmmoController ammoController;
-    private bool planeSelectEnabled = true;
+    public int numberOfTargets = 5;
 
     void Update()
     {
-        if (planeSelectEnabled && !planeSelectionDone && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (this.enabled && !planeSelectionDone && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
@@ -30,15 +31,34 @@ public class PlaneSelect : MonoBehaviour
                         p.gameObject.SetActive(false);
                     }
                     plane.gameObject.SetActive(true);
-                    planeSelectionDone = true;
-                    planeManager.enabled = false;
-                    raycastManager.enabled = false;
-                    StartButtonController.ShowStartButton();
-                    TargetBehavior.SpawnTargets();
-                    AmmoController.ShowAmmo();
-                    planeSelectEnabled = false;
+                    startButtonController.ShowStartButton();
+                    SpawnTargets(numberOfTargets, plane);
+                    ammoController.ShowAmmo();
+                    this.enabled = false;
                 }
             }
         }
+    }
+    public void SpawnTargets(int numberOfTargets, ARPlane plane)
+    {
+        for (int i = 0; i < numberOfTargets; i++)
+        {
+            Vector3 randomPosition = GetRandomPositionOnPlane(plane);
+            Instantiate(targetPrefab, randomPosition, Quaternion.identity);
+        }
+    }
+    private Vector3 GetRandomPositionOnPlane(ARPlane plane, float margin = 0f, float padding = 0f)
+    {
+        Vector3 planeCenter = plane.transform.position;
+        Vector3 planeExtents = new Vector3((plane.size.x / 2) - margin, 0, (plane.size.y / 2) - margin);
+
+        // Ensure that padding does not exceed the adjusted extents
+        padding = Mathf.Min(padding, planeExtents.x, planeExtents.z);
+
+        float randomX = Random.Range(-planeExtents.x + padding, planeExtents.x - padding);
+        float randomZ = Random.Range(-planeExtents.z + padding, planeExtents.z - padding);
+        float yPosition = planeCenter.y;
+
+        return new Vector3(randomX + planeCenter.x, yPosition, randomZ + planeCenter.z);
     }
 }
--- unity-ar_slingshot_game/Assets/Scripts/TargetBehavior.cs
+++ unity-ar_slingshot_game/Assets/Scripts/TargetBehavior.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class TargetBehavior : MonoBehaviour
 {
     public GameObject targetPrefab;
-    public int numberOfTargets = 5;
     public float minScale = 0.2f;
     public float maxScale = 0.5f;
     public float movementSpeed = 1.0f;
     public bool moveInXDirection = true;
     public bool moveInZDirection = true;
+    private ARPlane plane;
 
     private void Start()
     {
+        plane = GetComponent<ARPlane>();
+        
         float randomScale = Random.Range(minScale, maxScale);
         transform.localScale = new Vector3(randomScale, randomScale, randomScale);
     }
@@ -23,29 +26,5 @@ public class TargetBehavior : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, 0, moveZ) * movementSpeed * Time.deltaTime;
         transform.Translate(movement, Space.World);
-    }
-
-    public static void SpawnTargets(int numberOfTargets, ARPlane plane)
-    {
-        for (int i = 0; i < numberOfTargets; i++)
-        {
-            Vector3 randomPosition = GetRandomPositionOnPlane(plane);
-            Instantiate(targetPrefab, randomPosition, Quaternion.identity);
-        }
-    }
-    csharp
-    private Vector3 GetRandomPositionOnPlane(ARPlane plane, float margin = 0f, float padding = 0f)
-    {
-        Vector3 planeCenter = plane.transform.position;
-        Vector3 planeExtents = new Vector3((plane.size.x / 2) - margin, 0, (plane.size.y / 2) - margin);
-
-        // Ensure that padding does not exceed the adjusted extents
-        padding = Mathf.Min(padding, planeExtents.x, planeExtents.z);
-
-        float randomX = Random.Range(-planeExtents.x + padding, planeExtents.x - padding);
-        float randomZ = Random.Range(-planeExtents.z + padding, planeExtents.z - padding);
-        float yPosition = planeCenter.y;
-
-        return new Vector3(randomX + planeCenter.x, yPosition, randomZ + planeCenter.z);
     }
 }
